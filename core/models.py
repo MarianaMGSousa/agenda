2328a1ec73f57740b6cca2bbf8938d6bd925262b
Mariana Moreira <92062708+MarianaMGSousa@users.noreply.github.com>
2022-06-24 11:27:49 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -15,3 +15,6 @@ class Evento(models.Model):
         
     def __str__(self):
         return self.titulo
+    
+    def get_data_evento(self):  #para imprimir a data de uma maneira melhor, mais bonita
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Evento
 
 # Create your views here.
+def evento(request, titulo_evento):
+    consulta = evento.objects.get(titulo=titulo_evento)#os parametros do evento titulo
+    return HttpResponse('<h1>O evento será: {}</h1>'.format(consulta))
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario) #colocando um oarametro para a listagem, só vai aparecer a agenda de quem estiver logado
+    dados = {'eventos' :evento}
+    return render(request, 'agenda.html', dados)
+
+# def index(request): #se não colocarmos nada será redirecionado para o /agenda
+#     return redirect('/agenda/')
